9edc140d9529f2b68c03286c8e78cbbf2f52a97d
Aniket Sinha <37899215+aniketsinha0001@users.noreply.github.com>
2018-12-07 15:52:57 +0530

    Linear Searching in Arrays

---
+++ LinearSearch.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+
+	public static void main(String... a) {
+		int flag = 0;
+		int array[] = { 11, 15, 2, 14, 55, 24 };
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Element to Search!");
+		int item = sc.nextInt();
+		int i;
+		for (i = 0; i < array.length; i++) {
+			if (array[i] == item) {
+				System.out.println("Item Found in Array at Index: " + i);
+				flag = 1 + i;
+				break;
+			} else {
+				flag = 0;
+			}
+
+		}
+		if (flag != 0) {
+			System.out.println(item + " Item Found at Index " + i);
+		} else {
+			System.out.println("Not Found!");
+		}
+	}
+}
