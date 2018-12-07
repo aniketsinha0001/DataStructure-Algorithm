5a4a88042c93c895bfea51145191f4a88438836b
Aniket Sinha <37899215+aniketsinha0001@users.noreply.github.com>
2018-12-07 16:22:51 +0530

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -7,22 +7,27 @@ public class LinearSearch {
 		int array[] = { 11, 15, 2, 14, 55, 24 };
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the Element to Search!");
-		int item = sc.nextInt();
-		int i;
-		for (i = 0; i < array.length; i++) {
-			if (array[i] == item) {
-				System.out.println("Item Found in Array at Index: " + i);
-				flag = 1 + i;
-				break;
-			} else {
-				flag = 0;
-			}
+		try {
+			int item = sc.nextInt();
+			int i;
+			for (i = 0; i < array.length; i++) {
+				if (array[i] == item) {
+					System.out.println("Item Found in Array at Index: " + i);
+					flag = 1 + i;
+					break;
+				} else {
+					flag = 0;
+				}
 
+			}
+			if (flag != 0) {
+				System.out.println(item + " Item Found at Index " + i);
+			} else {
+				System.out.println("Not Found!");
+			}
+		} catch (Exception e) {
+			System.out.println("Please Enter Number Only!");
 		}
-		if (flag != 0) {
-			System.out.println(item + " Item Found at Index " + i);
-		} else {
-			System.out.println("Not Found!");
-		}
+
 	}
 }
