cb03bd5f8bb16dbc9d8dab24a5ddf773c364cebc
Aniket Sinha <37899215+aniketsinha0001@users.noreply.github.com>
2019-08-22 16:06:13 +0530

    CircularLinkedList Traversal

---
+++ CircularLinkedListTraversal.java
@@ -0,0 +1,46 @@
+package com.sinha.aniket.linkedlist;
+
+import java.util.Stack;
+
+public class CircularLinkedListTraversal {
+	Node head;
+	static class Node{
+		int data;
+		Node next;
+		
+		Node(int data){
+			this.data=data;
+		}
+	}
+	
+	private void displayList(){
+		Stack<Integer> s=new Stack<>();
+		Node temp=head;
+		while(temp!=null){
+			System.out.print(temp.data+" ");
+			temp=temp.next;
+			if(temp==head){
+				break;
+			}
+		}
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		CircularLinkedListTraversal cll=new CircularLinkedListTraversal();
+		Node n1=new Node(1);
+		Node n2=new Node(2);
+		Node n3=new Node(3);
+		Node n4=new Node(4);
+		Node n5=new Node(5);
+		
+		cll.head=n1;
+		n1.next=n2;
+		n2.next=n3;
+		n3.next=n4;
+		n4.next=n5;
+		n5.next=n1;
+		
+		cll.displayList();
+	}
+
+}
